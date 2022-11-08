3919bfb05c46b5b08a5ff018c3c5de9f537d4db2
Frostist <williamf1245@gmail.com>
2022-11-08 15:37:36 +0200

    Initial Commit

---
+++ TipApp/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  TipApp
+//
+//  Created by Will Frost on 2022/11/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var total = ""
+    @State var tipPercent = 15.0
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: "dollarsign.circle.fill")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                    .font(.title)
+                Text("Tip Calculator")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+            }
+            
+            HStack {
+                Text("$")
+                TextField("Amount", text: $total)
+            }.padding(.horizontal, 150.0)
+            
+            
+            HStack {
+                Slider(value: $tipPercent, in: 1...30, step: 1.0)
+                Text("\(Int(tipPercent))")
+                Text("%")
+            }.padding()
+            
+            if let totalNumber = Double(total) {
+                Text ("Tip Amount: $\((Double(total) ?? 0) * tipPercent / 100, specifier: "%0.2f")")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TipApp/TipAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipAppApp.swift
+//  TipApp
+//
+//  Created by Will Frost on 2022/11/08.
+//
+
+import SwiftUI
+
+@main
+struct TipAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
